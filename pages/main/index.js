9bea31782511ed144b35f55cf6a467189dc689ec
AlexOlga <bulbenko@yandex.ru>
2022-04-21 10:11:04 +0300

    feat: added work burger menu

--- pages/main/index.js
+++ pages/main/index.js
@@ -4,20 +4,14 @@ const blackout=document.querySelector('.blackout');
 const bodyTeg=document.body;
 const navLink=document.querySelector('.nav__list');
 const logo=document.querySelector('.logo');
-hamburger.addEventListener('click', ()=>{
+
+function toggleMenu(){
     menu.classList.toggle('nav-active');
     hamburger.classList.toggle('burger-active');
     blackout.classList.toggle('blackout-active');
     bodyTeg.classList.toggle('look');
     logo.classList.toggle('logo-active');
-
-    });
-    
-    navLink.addEventListener('click', ()=>{  
-        console.log('clic');
-        menu.classList.toggle('nav-active');
-    hamburger.classList.toggle('burger-active');
-    blackout.classList.toggle('blackout-active');
-    bodyTeg.classList.toggle('look');
-    logo.classList.toggle('logo-active');
-    } );
+}
+hamburger.addEventListener('click',toggleMenu);
+navLink.addEventListener('click',toggleMenu);
+blackout.addEventListener('click', toggleMenu);
